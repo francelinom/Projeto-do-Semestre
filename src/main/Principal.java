a6170995c9e35447f58d2168fe9604038cb07779
thiagodantas90 <43214554+thiagodantas90@users.noreply.github.com>
2018-10-16 14:45:43 -0300

    16/10

---
+++ src/main/Principal.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+public class Principal extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+       
+        Parent root = FXMLLoader.load(getClass().getResource("/visao/Principal.fxml"));
+        Scene scene = new Scene(root);
+        
+        primaryStage.centerOnScreen();
+        primaryStage.setHeight(Screen.getPrimary().getVisualBounds().getHeight());
+        primaryStage.setWidth(Screen.getPrimary().getVisualBounds().getWidth());
+ 
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("PDV Fácil");
+        primaryStage.show();
+    }
+ 
+}
