8dff15a973c3771a66c9a67480894924b3b27356
thiagodantas90 <43214554+thiagodantas90@users.noreply.github.com>
2018-10-17 15:14:42 -0300

    alteração dia 17/10 15:14

--- src/modelos/Venda.java
+++ src/modelos/Venda.java
@@ -5,19 +5,15 @@ public class Venda {
     private int itens;//mudar para lista
     private double totVenda;
     private int totItens;
-    //private String nome_cliente;
     private String dataVenda;
-    //private String formaPgto;
     
     
     //CONSTRUTOR PARA INSERIR
     public Venda(int itens, double totVenda, int totItens, String nome_cliente, String dataVenda, String formaPgto) {
         this.itens = itens;
         this.totVenda = totVenda;
-        this.totItens = totItens;
-  //      this.nome_cliente = nome_cliente;
-        this.dataVenda = dataVenda;
-       // this.formaPgto = formaPgto;
+        this.totItens = totItens;  
+        this.dataVenda = dataVenda;     
     }
 
     //CONSTRUTOR PARA CONSULTAR
@@ -57,15 +53,6 @@ public class Venda {
     public void setTotItens(int totItens) {
         this.totItens = totItens;
     }
-
-//    public String getNome_cliente() {
-//        return nome_cliente;
-//    }
-//
-//    public void setNome_cliente(String nome_cliente) {
-//        this.nome_cliente = nome_cliente;
-//    }
-
     public String getDataVenda() {
         return dataVenda;
     }
@@ -73,12 +60,4 @@ public class Venda {
     public void setDataVenda(String dataVenda) {
         this.dataVenda = dataVenda;
     }
-
-//    public String getFormaPgto() {
-//        return formaPgto;
-//    }
-//
-//    public void setFormaPgto(String formaPgto) {
-//        this.formaPgto = formaPgto;
-//    }
 }
